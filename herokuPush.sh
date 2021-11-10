38d0889c1fe0b794108cad6f6fbf2356c4f52365
Roman Gaponov <romangaponov@gmail.com>
2021-11-10 13:19:12 +0300

    add heroku

---
+++ herokuPush.sh
@@ -0,0 +1,11 @@
+heroku login
+
+heroku container:login
+
+docker container stop mafia-front
+docker container rm mafia-front
+docker build -t mafia-front .
+
+heroku container:push web --app mafia-front
+
+heroku container:release web --app mafia-front
